a26e39563e0e5d0bf730d70ff6cf0e707f2d769b
Rodolfo Abadia <rabadiaf@gmail.com>
2025-07-05 16:32:42 -0600

    Fix kubectl path and update deployment logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "Starting deployment..."
 #  Usa kubeconfig embebido
 export KUBECONFIG=/app/kubeconfig.embedded.yaml
 
-ansible-playbook /app/playbook.yml
+ansible-playbook /app/playbook.yml || true
